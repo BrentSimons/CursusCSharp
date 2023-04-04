b85fcdc0baeed6ed84e55ce579f0d7bd6baea6e4
BRSimons15 <r0878913@student.thomasmore.be>
2023-04-04 13:24:40 +0200

    Updates

---
+++ assets/Excercises_Exercise_three.md.62fb3a5a.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as r}from"./chunks/framework.c3acf437.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"Excercises/Exercise_three.md"}'),s={name:"Excercises/Exercise_three.md"};function c(a,o,n,i,_,p){return r(),t("div")}const x=e(s,[["render",c]]);export{m as __pageData,x as default};
---
+++ assets/Syntax.md.873d554c.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,N as l}from"./chunks/framework.c3acf437.js";const A=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"Syntax.md"}'),p={name:"Syntax.md"},o=l("",15),e=[o];function t(c,r,D,F,y,i){return a(),n("div",null,e)}const h=s(p,[["render",t]]);export{A as __pageData,h as default};
